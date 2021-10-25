5081994f5546ab9254e80066d208fa3e7bf560e3
jonssonmarie <89392551+jonssonmarie@users.noreply.github.com>
2021-10-25 10:22:34 +0200

    Exercis_01 info added

---
+++ Exercise/02/Exercise_01.py
@@ -0,0 +1,21 @@
+"""
+1. Swedish demographic data
+Go to Swedish-language wikipedia page Sveriges demografi.
+a) Read in the table under "Befolkningsstatistik sedan 1900" into a DataFrame
+b) Choose to do some EDA (exploratory data analysis) on this dataset. And draw some relevant graphs.
+c) Now we want to go backwards in time (before 1900) to see how population has changed in Sweden. 
+   Read in the table under history and keep the data of "Folkmängd" from 1570-1865.
+
+År	Folkmängd
+1570	900000
+1650	1225000
+1700	1485000
+1720	1350000
+1755	1878000
+1815	2465000
+1865	4099000
+d) Now concatenate this with the table from 1900 so that you have population data from 1570 to 2020. 
+   Note that you may need to clean the data in order for it to fit properly. 
+   Also you may be able to do this in several ways.
+e) Draw a graph of population data from 1570-2020.
+"""

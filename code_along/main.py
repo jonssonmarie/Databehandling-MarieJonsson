29788165fa9694f677e2251d98cb62d0985c6d3d
jonssonmarie <89392551+jonssonmarie@users.noreply.github.com>
2021-11-02 09:56:57 +0100

    upd from lecture

--- code_along/main.py
+++ code_along/main.py
@@ -1,2 +1,28 @@
+
 import pandas as pd
+from dash import dcc, html
+# dcc interaktiva saker, html för html element
+import dash 
+
+app = dash.Dash(__name__)
+
+
+app.layout = html.Div([
+    html.H1("Stocks viewer"),             # H1  = header one
+    html.H2("This is a cool app")
+])
+
+if __name__ == "__main__":
+    app.run_server(debug=True)
+
+
+
+
+
+
+
+
+
+
+
 

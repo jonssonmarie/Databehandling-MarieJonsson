392810449d8d4f7450104b3daf7d6448d13c2e8d
jonssonmarie <89392551+jonssonmarie@users.noreply.github.com>
2021-11-04 20:05:21 +0100

    small fix html names

--- Lab/lab_covid_task4.py
+++ Lab/lab_covid_task4.py
@@ -9,6 +9,7 @@ import pandas as pd
 import numpy as np
 from initial_analyse import analyse
 from many_plots import bar_plot
+from statistic_covid import statistic_eu_vaccin
 
 # paths to excel files
 who_path = r"../Data/WHO COVID-19 global table data November 1st 2021 at 5.02.45 PM.csv"
@@ -16,9 +17,10 @@ ecdc_vaccine_path = r"../Data/Data on COVID-19 vaccination in the EUEEA.csv"
 
 # read excel files
 covid_cases_who = pd.read_csv(who_path).reset_index()\
-    .rename(columns={"index": "Nation",
+    .rename(columns={"index": "Nation", "Cases - cumulative total": "Cases - cumulative total per 100000",
+                     "Deaths - cumulative total": "Deaths - cumulative total per 100000",
             "Cases - cumulative total per 100000 population": "Covid cases - cumulative total per 100000 population"})
-
+#
 vaccine_ecdc = pd.read_csv(ecdc_vaccine_path).iloc[1:, :]
 
 # call on initial_analyse.py
@@ -47,36 +49,51 @@ def sum_per_column(names, df):
 
 
 ecdc_vaccine_cases = vaccine_ecdc[["YearWeekISO", "FirstDose", "SecondDose", "Region", "ReportingCountry",
-                                 "TargetGroup", "Population"]]
-ecdc_vaccine = sum_per_column(country_list, ecdc_vaccine_cases)
+                                  "TargetGroup", "Population"]]
 
+# return df with "Region", total dose 1, total dose 2
+ecdc_vaccine = sum_per_column(country_list, ecdc_vaccine_cases)
+# return percentage for each Region and % dose 1, % dose 2
+precentage = statistic_eu_vaccin(country_list, ecdc_vaccine)
 
 # limit data for WHO-cases and drop first row
-who_cases = covid_cases_who[["Nation", "WHO Region", "Covid cases - cumulative total per 100000 population",
-                             "Deaths - cumulative total per 100000 population"]].iloc[1:, :].dropna()
+who_cases = covid_cases_who[["Nation", "WHO Region", "Cases - cumulative total per 100000",
+                             "Covid cases - cumulative total per 100000 population",
+                             "Deaths - cumulative total per 100000"]].iloc[1:, :].dropna()
 
 # split data for cumulative per 100000 population for easier plots
-who_cases_below = who_cases[(who_cases["Covid cases - cumulative total per 100000 population"] < 10000)
-                            & (who_cases["Covid cases - cumulative total per 100000 population"] > 5000)]
+who_cases_between = who_cases[(who_cases["Deaths - cumulative total per 100000"] < 500)
+                              & (who_cases["Deaths - cumulative total per 100000"] > 200)]
 
-who_cases_above = who_cases[(who_cases["Covid cases - cumulative total per 100000 population"] >= 10000)]
-who_cases_below_5000 = who_cases[(who_cases["Covid cases - cumulative total per 100000 population"] <= 5000)]
+who_cases_above = who_cases[(who_cases["Deaths - cumulative total per 100000"] >= 500)]
+who_cases_below_5000 = who_cases[(who_cases["Deaths - cumulative total per 100000"] <= 200) &
+                                 (who_cases["Deaths - cumulative total per 100000"] > 100)]
+who_cases_below_100 = who_cases[(who_cases["Deaths - cumulative total per 100000"] <= 100)]
 
 # plots
-bar_plot(who_cases_below, "Nation", ["Covid cases - cumulative total per 100000 population",
-                               "Deaths - cumulative total per 100000 population"],
-         "Nations with number of cumulative covid cases per 100000 below 10000 and above 5000 in the world", None,
-         r'Visualiseringar\covid_who_kumulativ_per100000_between_10000_to_5000_task4.html')
+bar_plot(who_cases_above, "Nation", ["Cases - cumulative total per 100000",
+                                     "Deaths - cumulative total per 100000"],
+         "Nations with number of cumulative covid cases per 100000 above 500 in the world ", None,
+         r'Visualiseringar\task_4_WHO_covid_cumulativ_deaths_per100000_above_500.html')
 
-bar_plot(who_cases_above, "Nation", ["Covid cases - cumulative total per 100000 population",
-                               "Deaths - cumulative total per 100000 population"],
-         "Nations with number of cumulative covid cases per 100000 above 10000 in the world ", None,
-         r'Visualiseringar\covid_who_kumulativ_per100000_above_10000_task4.html')
+bar_plot(who_cases_between, "Nation", ["Cases - cumulative total per 100000",
+                                       "Deaths - cumulative total per 100000"],
+         "Nations with number of cumulative covid cases per 100000 below 500 and above 200 in the world", None,
+         r'Visualiseringar\task_4_WHO_covid_cumulativ_deaths_per100000_between_200_to_500.html')
 
-bar_plot(who_cases_above, "Nation", ["Covid cases - cumulative total per 100000 population",
-                               "Deaths - cumulative total per 100000 population"],
-         "Nations with number of cumulative covid cases per 100000 below 5000 in the world ", None,
-         r'Visualiseringar\covid_who_kumulativ_per100000_below_5000_task4.html')
+bar_plot(who_cases_below_5000, "Nation", ["Cases - cumulative total per 100000",
+                                          "Deaths - cumulative total per 100000"],
+         "Nations with number of cumulative covid cases per 100000 below 200 in the world ", None,
+         r'Visualiseringar\task_4_WHO_covid_kumulativ_deaths_per100000_below_200.html')
+
+bar_plot(who_cases_below_100, "Nation", ["Cases - cumulative total per 100000",
+                                         "Deaths - cumulative total per 100000"],
+         "Nations with number of cumulative covid cases per 100000 below 100 in the world ", None,
+         r'Visualiseringar\task_4_WHO_covid_kumulativ_deaths_per100000_below_100.html')
 
 bar_plot(ecdc_vaccine, "ReportingCountry", ["FirstDose", "SecondDose", "Population"],
-         "First and second vaccin dose EU/EEA", None, r'Visualiseringar\vaccin_eu_eea_task4.html')
+         "First and second vaccin dose EU/EEA", None, r'Visualiseringar\task_4_EU_EEA_vaccine.html')
+
+bar_plot(precentage, "ReportingCountry", ["procent_dose1", "procent_dose2"], 
+         "% dose 1 and % dose 2 for each EU/EEA country", None,
+         r'Visualiseringar\task_4_EU_EEA_percent_task4.html')
